7f2fdb28af88fd778a738de94e45d603c6105144
twyle <lyceokoth@gmail.com>
2023-12-09 11:15:26 +0300

    updates landing page.

--- waitlist.js
+++
@@ -1,6 +0,0 @@
-const waitlistButton = document.querySelector('#waitlist-btn')
-
-waitlistButton.addEventListener('click', () => {
-    console.log('going to home page')
-    window.location.href = './index.html'
-})
